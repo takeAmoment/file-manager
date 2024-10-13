92f1ea9e28db12cbdf122f9f80b9f6bb6b8a802c
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-13 14:14:34 +0200

    refactor: refactor read and create funcs

--- src/fs/createFile.js
+++ src/fs/createFile.js
@@ -8,19 +8,21 @@ import { OPERATION_FAILED_MESSAGE } from '../variables/common.js'
 export const createFile = async (commandArgs) => {
   if(commandArgs.length > 1) {
     console.error('Invalid input')
+    return
   }
 
   const fileName = commandArgs[0]
   const dirName = process.cwd()
   const filePath = join(dirName, fileName)
 
-  const isFileExist = await checkIsFileExist(filePath)
-
-  if(isFileExist) {
-    console.error(`${OPERATION_FAILED_MESSAGE}. This fail already exists`)
-  }
-
   try {
+    const isFileExist = await checkIsFileExist(filePath)
+
+    if(isFileExist) {
+      console.error(`${OPERATION_FAILED_MESSAGE}. This fail already exists`)
+      return
+    }
+    
     writeFile(filePath, '')
   } catch (error) {
     console.error(OPERATION_FAILED_MESSAGE)
--- src/fs/readFile.js
+++ src/fs/readFile.js
@@ -8,30 +8,37 @@ import { OPERATION_FAILED_MESSAGE } from '../variables/common.js'
 export const readFile = async (commandArgs) => {
   if(commandArgs.length > 1) {
     console.error('Invalid input')
+    return
   }
+
   const fileName = commandArgs[0]
   const dirName = process.cwd()
   const filePath = join(dirName, fileName)
   let result = ''
 
-  const isFileExist = checkIsFileExist(filePath)
+  try { 
+    const isFileExist = await checkIsFileExist(filePath)
 
-  if(!isFileExist) {
-   console.error(`${OPERATION_FAILED_MESSAGE}. File does not exist`)
+    if(!isFileExist) {
+     console.error(`${OPERATION_FAILED_MESSAGE}. File does not exist`)
+     return
+    }
+
+    const readableStream = createReadStream(filePath,  { encoding: 'utf8' })
+  
+    readableStream.on('data', (data) => {
+      result += data
+    })
+  
+    readableStream.on('error', () => {
+      console.error(OPERATION_FAILED_MESSAGE)
+    })
+  
+    readableStream.on('end', () => {
+      console.log('Result:\n', result)
+    })
+  } catch (error) {
+    
   }
-
-  const readableStream = createReadStream(filePath, 'utf8')
-
-  readableStream.on('data', (data) => {
-    result += data
-  })
-
-  readableStream.on('error', () => {
-    console.error(OPERATION_FAILED_MESSAGE)
-  })
-
-  readableStream.on('end', () => {
-    console.log('Result:\n', result)
-  })
 
 }
