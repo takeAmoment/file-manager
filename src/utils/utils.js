97bf0ba275927d7fff81921fac6270886f988b7e
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-13 15:21:38 +0200

    feat: add cp command

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import { access, constants } from 'fs/promises'
+import path from 'path'
 
 import { changeDirectory } from '../navigation/changeDirectory.js'
 import { getList } from '../navigation/getList.js'
@@ -6,14 +7,14 @@ import { moveUp } from '../navigation/moveUp.js'
 import { readFile } from '../fs/readFile.js'
 import { createFile } from '../fs/createFile.js'
 import { renameFile } from '../fs/renameFile.js'
-
+import { copyFile } from '../fs/copyFile.js'
 
 export const parseCommand = (command) => {
   const commandArr = command.split(' ')
   const commandName = commandArr[0]
   const commandArgs = commandArr.slice(1)
 
-  return  { commandName, commandArgs}
+  return { commandName, commandArgs }
 }
 
 export const checkCommand = (command) => {
@@ -26,17 +27,20 @@ export const checkCommand = (command) => {
     case 'cd':
       changeDirectory(commandArgs)
       break
-    case 'ls': 
+    case 'ls':
       getList()
       break
     case 'cat':
-    readFile(commandArgs)
-    break
+      readFile(commandArgs)
+      break
     case 'add':
       createFile(commandArgs)
       break
     case 'rn':
       renameFile(commandArgs)
+      break
+    case 'cp':
+      copyFile(commandArgs)
       break
     default:
       console.error('Invalid input:', command)
@@ -50,4 +54,21 @@ export const checkIsFileExist = async (filePath) => {
   } catch (error) {
     return false
   }
+}
+
+export const checkIsPathAbsolute = (pathName) => {
+  path.isAbsolute(pathName)
+}
+
+export const createFilePath = (workingDirectory, filePath) => {
+  const filePathIsAbsolute = checkIsPathAbsolute(filePath)
+  let fullFilePath
+
+  if (filePathIsAbsolute) {
+    fullFilePath = filePath
+  } else {
+    fullFilePath = path.resolve(workingDirectory, filePath)
+  }
+
+  return fullFilePath
 }
