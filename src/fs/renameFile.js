455dc77259d57709d4a00befda1491da2cfd5a32
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-14 19:09:55 +0200

    fix: fix rn command

--- src/fs/renameFile.js
+++ src/fs/renameFile.js
@@ -1,19 +1,19 @@
-import { join } from 'path'
+import path from 'path'
 import { rename } from 'fs/promises'
 
-import { checkIsFileExist } from '../utils/utils.js'
+import { checkIsFileExist, createFilePath } from '../utils/utils.js'
 
 export const renameFile = async (commandArgs) => {
-  if(commandArgs.length > 2) {
+  if(commandArgs.length !== 2) {
     console.error('Invalid input')
     return
   }
 
-  const dirName = process.cwd()
-  const originFileName = commandArgs[0]
-  const newFileName = commandArgs[1]
-  const oldFilePath = join(dirName, originFileName)
-  const newFilePath = join(dirName, newFileName)
+  const workingDirectory = process.cwd()
+  const sourcePath = path.normalize(commandArgs[0])
+  const newFileName = path.normalize(commandArgs[1])
+  const oldFilePath = createFilePath(workingDirectory, sourcePath)
+  const newFilePath = createFilePath(workingDirectory, newFileName)
 
   try {
     const isFileExist = await checkIsFileExist(oldFilePath)
